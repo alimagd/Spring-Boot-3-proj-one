386482db9a4a7bd931dc8e5428ff4d1512f1f03a
AliMaGD <mailofmajidi@gmail.com>
2023-04-23 07:10:18 +0100

    Initial commit

---
+++ src/test/java/com/magd/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magd.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
